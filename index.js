6a09ca341f061e740d5b67fe3fba0370a601845f
Catherine Bronzo <cbronzo@mac.com>
2021-02-01 23:22:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 // Write your solution here!
 
 const  = ["Milo", "Otis", "Garfield"]
+const  = ["Milo", "Otis", "Garfield"]
+const  = ["Milo", "Otis", "Garfield"]
+const  = ["Milo", "Otis", "Garfield"]
